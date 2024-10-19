f09c0b7a3e8b6fd4863e9aefe0b7553533c5ae8d
kyls <kyls2606@gmail.com>
2024-10-19 22:31:55 +0700

    feat: update flow witdraw

--- ViewModels/WithdrawRequestViewModel.cs
+++ ViewModels/WithdrawRequestViewModel.cs
@@ -2,7 +2,6 @@ namespace OkCoin.API.ViewModels;
 
 public class WithdrawRequestViewModel
 {
-    public string Address { get; set; } = null!;
     public decimal Amount { get; set; }
     public string? Note { get; set; }
 }
