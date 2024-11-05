3671b68cfa17a3ecb0e9ee12283c627b5a5e2d30
Rooster <nguyenthoai06011990@gmail.com>
2024-11-05 12:17:50 +0700

    upate

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 docker buildx build --platform linux/amd64 -t metafi/ok-coin-api:0.1.0 --load .
 
-docker buildx build --platform linux/amd64,linux/arm64 -t metafi/ok-coin-api:0.1.0 --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t metafi/ok-coin-api:0.1.1 --push .
 
-docker push metafi/ok-coin-api:0.1.0
+docker push metafi/ok-coin-api:0.1.1
