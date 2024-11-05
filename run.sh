ef28f81bf59fc285ee8f7a26d34db4b869d6955a
Rooster <nguyenthoai06011990@gmail.com>
2024-11-05 12:57:57 +0700

    update k8s

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 
 
-docker buildx build --platform linux/amd64 -t metafi/ok-coin-api:0.1.0 --load .
+docker buildx build --platform linux/amd64 -t metafi/ok-coin-api:0.1.3 --load .
 
-docker buildx build --platform linux/amd64,linux/arm64 -t metafi/ok-coin-api:0.1.1 --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t metafi/ok-coin-api:0.1.3 --push .
 
-docker push metafi/ok-coin-api:0.1.1
+docker push metafi/ok-coin-api:0.1.3
